92801f4f2fbc63aefbc7da8738993b5067519823
bianzheng123 <1317257555@qq.com>
2021-03-14 06:15:20 +0000

    modify the prepare sample structure

--- batch_run.py
+++ batch_run.py
@@ -27,9 +27,9 @@ short_config_fname_arr = ['1_kmeans_independent_16', '1_kmeans_multiple_16', '1_
 
 if __name__ == '__main__':
     # deep gist glove imagenet sift
-    ds_fname = 'sift'
+    ds_fname = 'siftsmall'
     _type = 'nn_classification'  # pq_nn nn_classification counting_index
-    base_config_dir = '/home/zhengbian/NN_as_Classification/config/%s/big_ds/' % _type
+    base_config_dir = '/home/zhengbian/NN_as_Classification/config/%s/small_ds/' % _type
     long_config_dir = base_config_dir + ds_fname + '.json'
 
     # short_config_fname_arr = ['1_knn_random_projection_16', '8_knn_random_projection_16']
@@ -37,6 +37,8 @@ if __name__ == '__main__':
     #                           '4_knn_kmeans_256', '4_knn_random_projection_256', '4_knn_lsh_256']
     short_config_fname_arr = ['1_kmeans_multiple_256', '1_knn_256', '1_knn_kmeans_multiple_256',
                               '1_knn_lsh_256', '1_knn_random_projection_256']
+    # short_config_fname_arr = ['1_kmeans_multiple_16', '1_knn_16', '1_knn_kmeans_multiple_16',
+    #                           '1_knn_lsh_16', '1_knn_random_projection_16']
     # short_config_fname_arr = ['1_random_projection_64']
     # short_config_fname_arr = ['1_knn_16', '8_knn_16']
     # short_config_fname_arr = ['1_e2lsh_16', '8_e2lsh_16']
--- sample_config/nn_classification/batch_run.py
+++ sample_config/nn_classification/batch_run.py
@@ -3,8 +3,40 @@ import json
 if __name__ == '__main__':
     with open('/home/zhengbian/NN_as_Classification/sample_config/nn_classification/short_term_config.json', 'r') as f:
         config = json.load(f)
-    save_base_dir = '/home/zhengbian/NN_as_Classification/config/nn_classification/big_ds'
+    save_base_dir = '/home/zhengbian/NN_as_Classification/config/nn_classification/small_ds'
     save_fname_content_m = [
+        {
+            "type": "knn",
+            "build_graph": {
+            },
+            "graph_partition": {
+                "type": "parhip"
+            }
+        },
+        {
+            "type": "knn_random_projection",
+            "build_graph": {
+            },
+            "graph_partition": {
+                "type": "parhip"
+            }
+        },
+        {
+            "type": "knn_lsh",
+            "build_graph": {
+            },
+            "graph_partition": {
+                "type": "parhip"
+            }
+        },
+        {
+            "type": "knn_kmeans",
+            "build_graph": {
+            },
+            "graph_partition": {
+                "type": "parhip"
+            }
+        },
         {
             "type": "knn_kmeans_multiple",
             "build_graph": {
@@ -16,7 +48,7 @@ if __name__ == '__main__':
     ]
     for tmp_config in save_fname_content_m:
         config['dataset_partition'] = tmp_config
-        config['n_cluster'] = 256
+        config['n_cluster'] = 16
         config['n_instance'] = 1
         with open('%s/%d_%s_%d.json' % (save_base_dir, config['n_instance'], tmp_config['type'], config['n_cluster']),
                   'w') as f:
